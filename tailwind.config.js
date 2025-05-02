8dad6cd609a36c77dc5caff4fee6fa604501841a
nybia <adalel@my.bcit.ca>
2025-05-01 21:21:09 -0700

    Readd necessary code

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                heading: ['Quicksand', 'sans-serif'],
+                body: ['Open Sans', 'sans-serif'],
+            },
+        },
+    },
+}
